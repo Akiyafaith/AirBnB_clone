a12903413f00e04aea6c75795a25bbd13a7a26c7
Akiyafaith <akiyafaith@gmail.com>
2023-05-15 14:32:34 -0700

    update

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,7 @@ from datetime import datetime
 #from models import storage
 
 
-class BaseModel:
+class BaseModel():
     """define a class Basemodel with attributes and methods for other classes
     """
     def __init__(self, *args, **kwargs):
@@ -20,11 +20,11 @@ class BaseModel:
 
         if kwargs:
             for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(
-                        value, "%Y-%m-%dT%H:%M:%S.%f"))
-                elif key != '__class__':
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                     setattr(self, key, value)
+
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -44,7 +44,7 @@ class BaseModel:
     def to_dict(self):
         """define a method to generate a dictionary
         representation of an instance"""
-        obj_dict = dict(self.__dict__.copy())
+        obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
